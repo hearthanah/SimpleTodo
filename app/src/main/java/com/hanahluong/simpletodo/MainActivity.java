6a727f236f5034cfe79f47d81a7f75e4a4f4ceba
Hanah Luong <hanahluong@Hanahs-MacBook-Pro.local>
2016-06-21 02:02:15 -0500

    first commit

---
+++ app/src/main/java/com/hanahluong/simpletodo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.hanahluong.simpletodo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemAdapter;
+    ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvItems = (ListView) findViewbyId(R.id.lvItems);
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First Item");
+        items.add("Second Item");
+    }
+}
