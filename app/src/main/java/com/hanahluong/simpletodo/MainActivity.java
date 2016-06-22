db5591bb78b3c73ae0f0dc481986184281fa0232
Hanah Luong <hanahluong@Hanahs-MacBook-Pro.local>
2016-06-21 22:35:53 -0500

    Cleaned up files

--- app/src/main/java/com/hanahluong/simpletodo/MainActivity.java
+++ app/src/main/java/com/hanahluong/simpletodo/MainActivity.java
@@ -27,12 +27,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lvItems = (ListView)findViewById(R.id.lvItems);
-//        items = new ArrayList<>();
         readItems();
         itemsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
-//        items.add("First Item");
-//        items.add("Second Item");
         setupListViewListener();
     }
 
